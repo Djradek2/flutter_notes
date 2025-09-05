be6c1aadc78a41a1219b623cf1ad48d62ff2fd54
Metroxx <lubos2003@gmail.com>
2025-09-05 10:38:15 +0200

    AI transpile

--- springversion/src/main/java/notesbackend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package notesbackend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    // No additional methods needed to fetch all
-}
